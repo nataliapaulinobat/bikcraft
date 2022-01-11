a2fbeba787931895b7ad3a8107e0089bc08fca60
Natalia Paulino <91961307+nataliapaulinobat@users.noreply.github.com>
2022-01-11 12:31:09 -0300

    js

--- bikcraft/js/script.js
+++ bikcraft/js/script.js
@@ -1,1 +1,22 @@
-console.log("teste");
+//ativar links do Menu
+const links = document.querySelectorAll('.header-menu a');
+
+function ativarLink(link){
+   const url= location.href;
+   const href = link.href
+
+if (url.includes(href)){
+    link.classList.add("ativo");
+}
+}
+links.forEach(ativarLink);
+//ativar items do orçamento
+
+const parametros =  new URLSearchParams (location.search);
+function ativarProduto(parametro){
+    const elemento = document.getElementById(parametro);
+    if(elemento){
+        elemento.ckecked = true;
+    }
+}
+parametros.forEach(ativarProduto);

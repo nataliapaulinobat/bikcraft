35e9f50fe9bd8e7dc1d5026760b6cc38f986d314
Natalia Paulino <91961307+nataliapaulinobat@users.noreply.github.com>
2022-01-11 17:12:48 -0300

    css e js

--- bikcraft/js/script.js
+++ bikcraft/js/script.js
@@ -41,3 +41,27 @@ function eventosPerguntas(pergunta){
 
 perguntas.forEach(eventosPerguntas);
 console.log(perguntas);
+
+// galeria de bicicleta
+const galeria = document.querySelectorAll(".bicicleta-imagens img");
+const galeriaContainer = document.querySelector(".bicicleta-imagens");
+
+function trocarImagem(event) {
+  const img = event.currentTarget;
+  const media = matchMedia("(min-width: 1000px)").matches;
+  if (media) {
+    galeriaContainer.prepend(img);
+  }
+}
+
+function eventosGaleria(img) {
+  img.addEventListener("click", trocarImagem);
+}
+
+galeria.forEach(eventosGaleria);
+
+
+//animação
+if(window.SimpleAnime){
+    new SimpleAnime();
+}
